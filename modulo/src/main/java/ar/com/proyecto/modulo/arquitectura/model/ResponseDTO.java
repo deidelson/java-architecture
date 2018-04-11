a91e169c6e6dfdf00236533bfb9441136c451715
deidelson93 <deidelson93@gmail.com>
2018-04-11 15:20:52 -0300

    Agregado el objeto a la respuesta

--- modulo/src/main/java/ar/com/proyecto/modulo/arquitectura/model/ResponseDTO.java
+++ modulo/src/main/java/ar/com/proyecto/modulo/arquitectura/model/ResponseDTO.java
@@ -1,14 +1,16 @@
 package ar.com.proyecto.modulo.arquitectura.model;
 
-public class ResponseDTO extends ArqDTO {
+public class ResponseDTO<T> extends ArqDTO {
 
+    private T objeto;
     public String response;
 
     public ResponseDTO() {
     }
 
-    public ResponseDTO(String response) {
+    public ResponseDTO(String response, T objeto) {
         this.response = response;
+        this.objeto = objeto;
     }
 
     public String getResponse() {
@@ -17,5 +19,13 @@ public class ResponseDTO extends ArqDTO {
 
     public void setResponse(String response) {
         this.response = response;
+    }
+
+    public T getObjeto() {
+        return objeto;
+    }
+
+    public void setObjeto(T objeto) {
+        this.objeto = objeto;
     }
 }
