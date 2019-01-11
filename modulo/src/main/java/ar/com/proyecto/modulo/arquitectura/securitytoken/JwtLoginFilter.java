0c803b00b044c84e427c90f53929a4ae8238324c
Danilo Eidelson <deidelson93@gmail.com>
2019-01-11 18:34:00 -0300

    Atributo como privado

--- modulo/src/main/java/ar/com/proyecto/modulo/arquitectura/securitytoken/JwtLoginFilter.java
+++ modulo/src/main/java/ar/com/proyecto/modulo/arquitectura/securitytoken/JwtLoginFilter.java
@@ -24,7 +24,7 @@ import java.util.Collections;
 public class JwtLoginFilter extends AbstractAuthenticationProcessingFilter {
 
     @Autowired
-    TokenProvider tokenProvider;
+    private TokenProvider tokenProvider;
 
     @Value("${token.headerName}")
     private String headerName;
